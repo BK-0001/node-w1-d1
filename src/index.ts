e598ea493cf178dae704897ec5ec7436c85af4b9
Bart Kang <bart.kang.0719@gmail.com>
2024-07-03 11:35:46 -0700

    send response back to client

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,16 @@ import { createServer } from "http";
 const PORT = 3000;
 const HOST = "localhost"; // 127.0.0.1, ::1
 
+// Content Type eg
+// 1. text/plain
+// 2. text/html
+// 3. application/json
 const server = createServer();
+
+server.on("request", (req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end("Hello World");
+});
 
 server.listen(PORT, HOST, () => {
   console.log(`[server]: listening at http://${HOST}:${PORT}`);
